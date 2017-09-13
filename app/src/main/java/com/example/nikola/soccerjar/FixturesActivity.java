3f89eeec92190eac7ea591a6feca91ce4b227cb6
joysoi <nikola.joysoi@gmail.com>
2017-09-13 10:42:31 -0400

    add fixtures activity

---
+++ app/src/main/java/com/example/nikola/soccerjar/FixturesActivity.java
@@ -0,0 +1,12 @@
+package com.example.nikola.soccerjar;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nikola on 9/12/2017.
+ */
+
+public class FixturesActivity extends AppCompatActivity {
+
+
+}
