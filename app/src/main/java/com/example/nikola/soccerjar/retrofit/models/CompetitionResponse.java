3d707ede2f4aa73803ce3fac58ce11dd3cd099fb
joysoi <nikola.joysoi@gmail.com>
2017-09-26 23:26:41 -0400

    competition retrofit response update

---
+++ app/src/main/java/com/example/nikola/soccerjar/retrofit/models/CompetitionResponse.java
@@ -0,0 +1,22 @@
+package com.example.nikola.soccerjar.retrofit.models;
+
+/**
+ * Created by Nikola on 7/3/2017.
+ */
+
+public class CompetitionResponse {
+
+
+    private String caption;
+
+    private int id;
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public int get_id() {
+        return id;
+    }
+
+}
