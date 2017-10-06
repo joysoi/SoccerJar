0cef257ba94c77f94c3cbdd89f8b3c1e738505fa
joysoi <nikola.joysoi@gmail.com>
2017-10-06 13:44:38 -0400

    added MVP

---
+++ app/src/main/java/com/example/nikola/soccerjar/MainPresenter.java
@@ -0,0 +1,78 @@
+package com.example.nikola.soccerjar;
+
+import com.example.nikola.soccerjar.retrofit.ApiManager;
+import com.example.nikola.soccerjar.retrofit.ApiService;
+import com.example.nikola.soccerjar.retrofit.models.CompetitionResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by Nikola on 10/2/2017.
+ */
+
+public class MainPresenter {
+
+    List<CompetitionResponse> list;
+    MainView view;
+
+    public void getCompetitionView() {
+
+        if (view != null) {
+            view.showProgressDialog();
+        }
+
+        ApiManager.getClient().create(ApiService.class).getCompetitions().enqueue(new Callback<List<CompetitionResponse>>() {
+            @Override
+            public void onResponse(Call<List<CompetitionResponse>> call, Response<List<CompetitionResponse>> response) {
+                if (response.isSuccessful()) {
+                    List<CompetitionResponse> competitionListResponse = response.body();
+                    final List<CompetitionResponse> responseList = new ArrayList();
+                    for (CompetitionResponse competitionResponse : competitionListResponse) {
+                        String[] strList = new String[]{"445", "446", "447", "449", "452", "455", "456", "459"};
+                        int[] intList = new int[strList.length];
+                        for (int i = 0; i < strList.length; i++) {
+                            try {
+                                intList[i] = Integer.parseInt(strList[i]);
+                            } catch (NumberFormatException nfe) {
+                                nfe.printStackTrace();
+                            }
+                        }
+                        for (int anIntList : intList) {
+                            if (competitionResponse.get_id() == anIntList) {
+                                responseList.add(competitionResponse);
+                            }
+                            if (view != null) {
+                                view.getCompetitionsList(responseList);
+                                view.dismissProgressDialog();
+                            }
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<CompetitionResponse>> call, Throwable t) {
+                if (view != null) {
+                    view.unsuccesfulResponse();
+                    view.dismissProgressDialog();
+                }
+            }
+        });
+
+
+    }
+
+    public void registerView(MainView mainView) {
+        this.view = mainView;
+    }
+
+    public void unRegisterView() {
+        this.view = null;
+    }
+
+}
