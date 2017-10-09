0d889f1c83c7f06636e18022d92bf54d381e5605
joysoi <nikola.joysoi@gmail.com>
2017-10-09 17:51:29 -0400

    repostioning the activity

---
+++ app/src/main/java/com/example/nikola/soccerjar/fragments/FixturesActivity.java
@@ -0,0 +1,27 @@
+package com.example.nikola.soccerjar.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.nikola.soccerjar.R;
+
+
+public class FixturesActivity extends AppCompatActivity {
+
+    public int id;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fixtures);
+
+        Intent intent = getIntent();
+        id = intent.getIntExtra("id", 0);
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, new FixturesFragment().newInstance(id)).commit();
+
+
+    }
+}
